5fd648c307c764b5f38ab0a77610f3c335a00338
Randall Winkhart <idgr@tutanota.com>
2025-09-28 03:29:27 -0400

    Remove "bash" from removals

--- removals.go
+++ removals.go
@@ -1,30 +1,18 @@
 package main
 
 import (
-	"os/exec"
-
 	"github.com/rwinkhart/go-boilerplate/front"
-	"github.com/rwinkhart/go-boilerplate/other"
 )
 
 func removals() {
 	for {
 		var doAll bool
-		choice := front.InputMenuGen("Application to remove:", []string{"back", "all", "bash", "linux-cachyos-lts"})
+		choice := front.InputMenuGen("Application to remove:", []string{"back", "all", "linux-cachyos-lts"})
 		var target string
 		switch choice {
 		case 1:
 			return
 		case 2:
-			cmd := exec.Command("pacman", "-Rcns", "bash", "linux-cachyos-lts", "--noconfirm")
-			err := cmd.Run()
-			if err != nil {
-				other.PrintError("Failed to perform application removals", 1)
-			}
-			doAll = true
-		case 3:
-			target = "bash"
-		case 4:
 			target = "linux-cachyos-lts"
 		}
 		if !doAll {
